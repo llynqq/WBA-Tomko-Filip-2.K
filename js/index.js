3e59fc0b07d2b265d573b65ffd9180d5a21f2273
llynqq <filip.tomko6@gmail.com>
2022-06-09 09:30:44 +0200

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,51 @@
-console.log("HIT IT")
-function sumNumbs(){
+// console.log("HIT IT")
+// function sumNumbs() {
+//     let a = 6;
+//     let b = 9;
+//     let sum = a * b + a + b
+//     console.log("Num = " + sum)
+// }
+// sumNumbs();
+
+// let list = ["pes", "kocka", "lev"]
+
+// list.forEach(zvire => {
+//     console.log("Polozka z pole: " + zvire)
+// });
+
+
+// for (let x = 0; x < 4; x++) {
+//     if (x > 1) {
+//         console.log("ahoj")
+//     }
+//     else{
+//         console.log("hello")
+//     }    
+// }
+
+console.log("Ahoj")
+function sumNumbs() {
     let a = 6;
     let b = 9;
     let sum = a * b + a + b
-    console.log("Num = " + sum)
+    console.log(`Num = ${sum}`)
 }
 sumNumbs();
+
+
+const demo = document.getElementById("demo")
+function h1Click() {
+    demo.innerHTML = "DOM JS Text";
+}
+
+$(document).ready(function(){
+    $("h1").click(function(){
+      $("h1").hide();
+    });
+  });
+
+
+  
+document.getElementById('click').onclick = function() {
+    console.log("button was clicked");
+}
